1986f7ebfbcf40a27f77f6198057040d733ead0d
BarucoMarco2091 <108827736+BarucoMarco2091@users.noreply.github.com>
2024-10-21 18:55:30 -0300

    dom

---
+++ script/script.js
@@ -0,0 +1,49 @@
+const cardData = [
+    {
+        image: "assets/images/desktop/image-deep-earth.jpg",
+        description: "DEEP EARTH",
+    },
+    {
+        image: "assets/images/desktop/image-night-arcade.jpg",
+        description: "NIGHT ARCADE",
+    },
+    {
+        image: "assets/images/desktop/image-soccer-team.jpg",
+        description: "SOCCER TEAM VR",
+    },
+    {
+        image: "assets/images/desktop/image-from-above.jpg",
+        description: "FROM UP ABOVE VR",
+    },
+    {
+        image: "assets/images/desktop/image-pocket-borealis.jpg",
+        description: "POCKET BOREALIS",
+    },
+    {
+        image: "assets/images/desktop/image-curiosity.jpg",
+        description: "THE CURIOSITY",
+    },
+    {
+        image: "assets/images/desktop/image-fisheye.jpg",
+        description: "MAKE IT FISHEYE",
+    },
+]
+
+function loadCards() {
+    const cardGrid = document.querySelector('.card-grid');
+    cardData.forEach((item) => {
+        const newCardGrid = document.createElement('div');
+        newCardGrid.className = 'new-card-grid';
+        const newCard = document.createElement('div');
+        newCard.className = 'new-card';
+        newCard.innerHTML =
+            `
+            <img src="${item.image}" alt="card-img">
+            <p>${item.description}</p>
+            `
+        cardGrid.appendChild(newCardGrid);
+        newCardGrid.appendChild(newCard);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', loadCards());
